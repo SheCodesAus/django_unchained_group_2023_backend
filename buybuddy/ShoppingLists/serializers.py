54a44d42293c3e7f44a84f5221cf0d27b56603d8
Yessy Lee <107485501+YessyLee@users.noreply.github.com>
2023-03-21 00:57:02 +0800

    serializers.py added
    Simple serializers.py added
    
    Added 2 files (urls.py and views.py) starter codes and import only. Both need work.

---
+++ buybuddy/ShoppingLists/serializers.py
@@ -0,0 +1,52 @@
+from rest_framework import serializers
+from .models import Product, Collection, ShoppingList
+
+class ProductSerializer(serializers.ModelSerializer):
+    # id = serializers.ReadOnlyField()
+    # name = serializers.CharField(max_length=200)
+    # image_url = serializers.URLField()
+    # product_url = serializers.URLField()
+    # price = serializers.IntergerField()
+    # brand = serializers.CharField(max_length=100)
+    # notes = serializers.CharField(max_length=500)
+    # add_to_shoppinglist = serializers.BooleanField()
+    # user = serializers.ReadOnlyField (source="user_product")
+    
+    class Meta:
+        fields = ('id', 'name', 'image_url', 'product_url', 'price', 'brand', 'notes', 'add_to_shoppinglist')
+        model = Product   
+    
+    def create (self, validated_data):
+        return Product.objects.create (**validated_data)
+    
+    """ Ability to edit product either use def update or use RetrieveUpdateDestroyAPIView in view.py - is that correct?"""    
+   
+   
+
+class CollectionSerializer(serializers.ModelSerializer):
+    """Many to many relationship: Collection and product_id"""
+    product_id = ProductSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Collection
+        fields = ('id', 'name')
+        read_only_fields = ('product_id', 'user_id')
+        
+    def create(self, validated_data):
+        return Collection.objects.create(**validated_data)   
+    
+          
+        
+class ShoppingListSerializer(serializers.ModelSerializer):
+    
+    class Meta: 
+        model = ShoppingList
+        fields = ('total_cost')
+        read_only_fields = ('product_id', 'user_id')
+              
+    def create (self, validated_data):
+        return ShoppingList.objects.create (**validated_data)
+    
+    """Do we need @property to calculate total?"""
+        
+    
--- buybuddy/ShoppingLists/views.py
+++ buybuddy/ShoppingLists/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.http import Http404
+from rest_framework import status, generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Product, Collection, ShoppingList
+from .serializers import ProductSerializer, CollectionSerializer, ShoppingListSerializer
+#import permissions as well
 
-# Create your views here.
+# Create your views here, expand them
+
+# class ProductList(generics.ListCreateAPIView):
+    # 
+    # 
+# 
+# class ProductDetails(generics.RetrieveUpdateDestroyAPIView):
+    # 
+    # 
+    # 
+# class CollectionList(generics.RetrieveUpdateDestroyAPIView):
+    # 
+    # 
+    # 
+# class ShoppingList(generics.ListCreateAPIView):
