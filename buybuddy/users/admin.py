c64e1b13b54129251dfaf0e2bbf11fac24a56c2b
FallowBlade <113985916+FallowBlade@users.noreply.github.com>
2023-03-21 18:18:18 +0800

    Updated Admin
    Create admin model for user admin

--- buybuddy/users/admin.py
+++ buybuddy/users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    add_form:
+    form =
+    model = 
+    list_display = ['email', 'username']
+
+    admin.site.register(CustomUserAdmin)
+    
+
