83bb0ff62bd7b653d1b9ad7a296255782e7310a4
niku153 <104482078+niku153@users.noreply.github.com>
2023-03-27 11:40:51 +0800

    Update urls.py

--- buybuddy/ShoppingLists/urls.py
+++ buybuddy/ShoppingLists/urls.py
@@ -4,7 +4,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('collections/', views.CollectionList.as_view(), name='collections'),
-    path('collections/<int:pk>', views.CollectionDetail.as_view(), name='collection-detail'),
+    path('collections/<int:pk>/', views.CollectionDetail.as_view(), name='collection-detail'),
     path('shoppinglists/', views.ShoppingList.as_view(), name='shoppinglists'),
 
 ]
